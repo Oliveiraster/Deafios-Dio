76966ac8553c6dd6d9852236e52cecf454896096
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2024-11-13 02:57:47 -0300

    desafio-validation

---
+++ desafio-validation/src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: ${({ disabled }) => (disabled ? '#ccc' : '#81259D')}; 
+    color: #FFF;
+
+    border: 1px solid ${({ disabled }) => (disabled ? '#ccc' : '#81259D')};
+    border-radius: 21px;
+
+    &:hover {
+        opacity: ${({ disabled }) => (disabled ? 1 : 0.6)}; 
+        cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+    }
+`;
